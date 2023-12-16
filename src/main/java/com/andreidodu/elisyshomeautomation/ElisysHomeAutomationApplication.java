f88a4492ea4fd0a0a1266a5778ec0e368566d962
Andrei Dodu <andrei.dodu@hotmail.it>
2023-12-16 21:49:19 +0100

    chore

--- src/main/java/com/andreidodu/elisyshomeautomation/ElisysHomeAutomationApplication.java
+++ src/main/java/com/andreidodu/elisyshomeautomation/ElisysHomeAutomationApplication.java
@@ -1,19 +1,11 @@
 package com.andreidodu.elisyshomeautomation;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.AdviceMode;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableFeignClients
-// @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class ElisysHomeAutomationApplication {
 
     public static void main(String[] args) {
