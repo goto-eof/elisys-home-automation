260d87dde11a713dfbb70c426489af34d0732692
Andrei Dodu <andrei.dodu@hotmail.it>
2023-12-17 07:06:14 +0100

    refactor

--- src/main/java/com/andreidodu/elisyshomeautomation/config/PersistenceConfiguration.java
+++ src/main/java/com/andreidodu/elisyshomeautomation/config/PersistenceConfiguration.java
@@ -3,11 +3,9 @@ package com.andreidodu.elisyshomeautomation.config;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EntityScan("com.andreidodu.elisyshomeautomation.entity")
-//@EnableTransactionManagement
 @EnableJpaRepositories("com.andreidodu.elisyshomeautomation.repository")
 public class PersistenceConfiguration {
 
