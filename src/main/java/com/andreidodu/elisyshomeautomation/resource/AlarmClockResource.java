4aed9886a6d4b89a8ab9ebadf8799e2cf9fd32ea
Andrei Dodu <andrei.dodu@hotmail.it>
2023-12-17 08:01:40 +0100

    refactor

--- src/main/java/com/andreidodu/elisyshomeautomation/resource/AlarmClockResource.java
+++ src/main/java/com/andreidodu/elisyshomeautomation/resource/AlarmClockResource.java
@@ -2,11 +2,7 @@ package com.andreidodu.elisyshomeautomation.resource;
 
 import com.andreidodu.elisyshomeautomation.dto.DeviceDTO;
 import com.andreidodu.elisyshomeautomation.dto.request.AlarmClockConfigurationRequestDTO;
-import com.andreidodu.elisyshomeautomation.dto.request.AlertRequestDTO;
-import com.andreidodu.elisyshomeautomation.dto.request.SensorConfigurationRequestDTO;
 import com.andreidodu.elisyshomeautomation.dto.response.AlarmClockConfigurationResponseDTO;
-import com.andreidodu.elisyshomeautomation.dto.response.MotionSensorConfigurationDTO;
-import com.andreidodu.elisyshomeautomation.dto.response.ResponseStatusDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
