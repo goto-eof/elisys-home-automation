9b5e0f09337533c2fcbb08cd0ba2e5648eaacf37
Andrei Dodu <andrei.dodu@hotmail.it>
2023-12-18 08:09:16 +0100

    chore: changed @version to instant type

--- src/main/java/com/andreidodu/elisyshomeautomation/entity/common/ModelCommon.java
+++ src/main/java/com/andreidodu/elisyshomeautomation/entity/common/ModelCommon.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
@@ -27,5 +28,6 @@ public abstract class ModelCommon {
     protected LocalDateTime lastModifiedDate;
 
     @Version
-    private Integer version;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Instant version;
 }
